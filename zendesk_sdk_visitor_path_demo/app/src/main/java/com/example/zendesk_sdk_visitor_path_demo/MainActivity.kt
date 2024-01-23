a91ad5b09f1934400ebca8252e2f5de3110c262b
Ian Arbuckle <ian.arbuckle18@gmail.com>
2024-01-23 16:09:50 +0000

    - [MFD-9262] Fixed crash for sending page view event * Moved the initialization code to the Application class * Wrapped the send page view event with a coroutine scope

--- zendesk_sdk_visitor_path_demo/app/src/main/java/com/example/zendesk_sdk_visitor_path_demo/MainActivity.kt
+++ zendesk_sdk_visitor_path_demo/app/src/main/java/com/example/zendesk_sdk_visitor_path_demo/MainActivity.kt
@@ -7,20 +7,18 @@ import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Button
-import android.widget.EditText
-import android.widget.Toast
 import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.core.view.WindowCompat
+import androidx.core.view.isVisible
+import com.google.android.material.card.MaterialCardView
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
-import com.google.android.material.snackbar.Snackbar
 import zendesk.android.Zendesk
 import zendesk.android.pageviewevents.PageView
 import zendesk.logger.Logger
-import zendesk.messaging.android.DefaultMessagingFactory
 
 class MainActivity : AppCompatActivity() {
 
-    val LOG_TAG = "[${this.javaClass.name}]"
+    private val LOG_TAG = "[${this.javaClass.name}]"
 
     private var coordinatorLayout: CoordinatorLayout? = null
 
@@ -31,19 +29,20 @@ class MainActivity : AppCompatActivity() {
         WindowCompat.setDecorFitsSystemWindows(window, false)
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.topAppBar))
-        coordinatorLayout = findViewById<CoordinatorLayout>(R.id.coordinatorLayout)
+        coordinatorLayout = findViewById(R.id.coordinatorLayout)
 
         // https://developer.zendesk.com/documentation/zendesk-web-widget-sdks/sdks/android/getting_started/#troubleshooting
         Logger.setLoggable(true)
 
-        // https://developer.zendesk.com/documentation/zendesk-web-widget-sdks/sdks/android/getting_started/#initialize-the-sdk
-        findViewById<Button>(R.id.InitButton).setOnClickListener {
-            initializeZendesk()
-        }
+        findViewById<MaterialCardView>(R.id.InitCard).isVisible = false
 
         // https://developer.zendesk.com/documentation/zendesk-web-widget-sdks/sdks/android/getting_started/#show-the-conversation
         findViewById<Button>(R.id.StartButton).setOnClickListener {
             Zendesk.instance.messaging.showMessaging(this)
+        }
+
+        findViewById<Button>(R.id.sendPageEventButton).setOnClickListener {
+            addVisitorPath()
         }
     }
 
@@ -66,34 +65,16 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    // SDK related methods
-
-    private fun initializeZendesk() {
-        // https://developer.zendesk.com/documentation/zendesk-web-widget-sdks/sdks/android/getting_started/#initialize-the-sdk
-        Zendesk.initialize(this, this.getString(R.string.channel_key), successCallback = { zendesk ->
-            Log.i(LOG_TAG, getString(R.string.msg_init_success))
-            coordinatorLayout?.let {
-                Snackbar.make(it, getString(R.string.msg_init_success), Snackbar.LENGTH_LONG).show()
-            }
-            addVisitorPath()
-        }, failureCallback = { error ->
-            // Tracking the cause of exceptions in your crash reporting dashboard will help to triage any unexpected failures in production
-            Log.e(LOG_TAG, "${getString(R.string.msg_init_error)}: $error")
-            coordinatorLayout?.let {
-                Snackbar.make(it, getString(R.string.msg_init_error), Snackbar.LENGTH_LONG).show()
-            }
-        }, messagingFactory = DefaultMessagingFactory())
-    }
-
     // https://developer.zendesk.com/documentation/zendesk-web-widget-sdks/sdks/android/advanced_integration/#visitor-path
     private fun addVisitorPath(
         url: String = "android:app",
         pageTitle: String = "Android landing page"
     ) {
-        // Create a `PageView` object
-        val pageView = PageView(url = url, pageTitle = pageTitle)
-        Log.d(LOG_TAG, "addVisitorPath: $pageView")
-        Zendesk.instance.sendPageView(pageView,
+    // Create a `PageView` object
+    val pageView = PageView(url = url, pageTitle = pageTitle)
+    Log.d(LOG_TAG, "addVisitorPath: $pageView")
+        Zendesk.instance.sendPageView(
+            pageView,
             successCallback = {
                 // Your success handling
                 Log.d(LOG_TAG, getString(R.string.log_visitor_path_success))
@@ -101,6 +82,7 @@ class MainActivity : AppCompatActivity() {
             failureCallback = { error ->
                 // Your error handling
                 Log.d(LOG_TAG, getString(R.string.log_visitor_path_error, error.message))
-        })
+            },
+        )
     }
 }
