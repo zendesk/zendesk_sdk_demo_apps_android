cb457642ee2ee8a79cd4c5088647b75e35a10e9e
dawid.seroka <dawid.seroka@zendesk.com>
2024-08-06 12:10:47 +0200

    sdk_metadata_demo update coreLibraryDesugaring to 2.0.4

--- zendesk_sdk_metadata_demo/app/build.gradle.kts
+++ zendesk_sdk_metadata_demo/app/build.gradle.kts
@@ -39,5 +39,5 @@ dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
-    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.3")
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
 }
