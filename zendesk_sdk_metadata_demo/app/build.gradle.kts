906a553f2b1c2dc5c6e21bc2dcc405e42f1747fa
Karolina Widz <karolina.widz@zendesk.com>
2025-06-09 12:39:00 +0200

    Update metadata_demo targetSdk and compileSdk to 36

--- zendesk_sdk_metadata_demo/app/build.gradle.kts
+++ zendesk_sdk_metadata_demo/app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.zendesk_sdk_metadata_demo"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.zendesk_sdk_metadata_demo"
         minSdk = 21
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
         vectorDrawables.useSupportLibrary = true
