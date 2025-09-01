5487f46737e94842ed685b84423c17f5b0ba6322
ian.arbuckle <ian.arbuckle@zendesk.com>
2025-09-01 09:58:48 +0100

    * Add auth delegate

--- zendesk_sdk_jwt_demo/app/src/main/java/com/example/zendesk_sdk_jwt_demo/MainApplication.kt
+++ zendesk_sdk_jwt_demo/app/src/main/java/com/example/zendesk_sdk_jwt_demo/MainApplication.kt
@@ -1,17 +1,34 @@
 package com.example.zendesk_sdk_jwt_demo
 
 import android.app.Application
+import android.content.Context
 import android.util.Log
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import zendesk.android.Zendesk
+import zendesk.android.ZendeskAuthenticationDelegate
 import zendesk.android.events.ZendeskEvent
 import zendesk.android.events.ZendeskEventListener
 import zendesk.messaging.android.DefaultMessagingFactory
 
 class MainApplication : Application() {
 
+    private var delegateScope = MainScope()
+
+    private var retries = 0
+
     override fun onCreate() {
         super.onCreate()
-
+        setupDelegate(
+            context = this,
+            successfulRetry = 3,
+            delay = 2000L,
+            jwt = this.resources.getString(R.string.jwt_token),
+        )
         // https://developer.zendesk.com/documentation/zendesk-web-widget-sdks/sdks/android/getting_started/#initialize-the-sdk
         Zendesk.initialize(this, this.getString(R.string.channel_key), successCallback = { zendesk ->
             Log.i(LOG_TAG, getString(R.string.msg_init_success))
@@ -20,6 +37,31 @@ class MainApplication : Application() {
             // Tracking the cause of exceptions in your crash reporting dashboard will help to triage any unexpected failures in production
             Log.e(LOG_TAG, "${getString(R.string.msg_init_error)}: $error")
         }, messagingFactory = DefaultMessagingFactory())
+    }
+
+    fun setupDelegate(
+        context: Context,
+        successfulRetry: Int,
+        delay: Long,
+        jwt: String,
+    ) {
+        retries = 0
+        delegateScope = CoroutineScope(Dispatchers.IO + SupervisorJob())
+        Zendesk.authenticationDelegate =
+            ZendeskAuthenticationDelegate { _, updateToken ->
+                delegateScope.launch {
+                    // Simulate delay of fetching JWT
+                    delay(delay)
+                    updateToken(
+                        if (retries >= successfulRetry.coerceIn(MIN_RETRY, MAX_RETRY)) {
+                            jwt
+                        } else {
+                            context.resources.getString(R.string.jwt_expired)
+                        },
+                    )
+                    retries++
+                }
+            }
     }
 
     private fun addEventListener() {
@@ -45,6 +87,8 @@ class MainApplication : Application() {
 
     companion object {
         private val LOG_TAG = "[${Companion::class.java.simpleName}]"
+        private const val MIN_RETRY = 0
+        private const val MAX_RETRY = 4
 
         /**
          * Private reference to the instance of [MainApplication], initialized during [onCreate].
