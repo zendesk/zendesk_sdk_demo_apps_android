182ae9f29b9de636bff8915ca4e54f3db0ef89c9
Ian Arbuckle <6011914+IanArb@users.noreply.github.com>
2025-12-15 09:28:35 +0000

    [MFD-13002] Add customised colours API to demo app (#49)
    * [MFD-13002] Add customised colours API to demo app
    
    * * PR feedback: Introduce new section for changing colors at runtime using a new card section
    
    * * PR feedback: Remove Hilt and make ZendeskManager an object singleton
    
    * * Remove redundant comment lines

--- zendesk_sdk_demo/app/src/main/java/com/example/zendesk_sdk_demo/MainActivity.kt
+++ zendesk_sdk_demo/app/src/main/java/com/example/zendesk_sdk_demo/MainActivity.kt
@@ -7,15 +7,15 @@ import android.view.MenuItem
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.core.content.ContextCompat
 import androidx.core.view.WindowCompat
 import androidx.core.view.isVisible
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
-import zendesk.android.Zendesk
+import zendesk.android.messaging.model.UserColors
 import zendesk.logger.Logger
+import zendesk.messaging.android.DefaultMessagingFactory
 
 class MainActivity : AppCompatActivity() {
-
-    private val LOG_TAG = "[${this.javaClass.name}]"
 
     private var coordinatorLayout: CoordinatorLayout? = null
 
@@ -33,11 +33,148 @@ class MainActivity : AppCompatActivity() {
 
         findViewById<Button>(R.id.InitButton).isVisible = false
 
-        // https://developer.zendesk.com/documentation/zendesk-web-widget-sdks/sdks/android/getting_started/#4-show-messaging
+        // https://developer.zendesk.com/documentation/zendesk-web-widget-sdks/sdks/android/ui_customization
         findViewById<Button>(R.id.StartButton).setOnClickListener {
-            Zendesk.instance.messaging.showMessaging(this)
+            ZendeskManager.showMessaging(context = this)
+        }
+
+        findViewById<Button>(R.id.ChangeColorsButton).setOnClickListener {
+            ZendeskManager.invalidate()
+            ZendeskManager.initialize(
+                context = this,
+                channelKey = this.getString(R.string.channel_key),
+                factory = DefaultMessagingFactory(
+                    userLightColors = lightColors(),
+                    userDarkColors = darkColors(),
+                )
+            )
         }
     }
+
+    private fun lightColors(): UserColors = UserColors(
+        primary = ContextCompat.getColor(
+            this,
+            R.color.user_primary_light,
+        ),
+        onPrimary = ContextCompat.getColor(
+            this,
+            R.color.user_on_primary_light,
+        ),
+        action = ContextCompat.getColor(
+            this,
+            R.color.user_action_light,
+        ),
+        onAction = ContextCompat.getColor(
+            this,
+            R.color.user_on_action_light,
+        ),
+        onMessage = ContextCompat.getColor(
+            this,
+            R.color.user_on_message_light,
+        ),
+        message = ContextCompat.getColor(
+            this,
+            R.color.user_message_light,
+        ),
+        onBusinessMessage = ContextCompat.getColor(
+            this,
+            R.color.user_on_business_message_light,
+        ),
+        businessMessage = ContextCompat.getColor(
+            this,
+            R.color.user_business_message_light,
+        ),
+        background = ContextCompat.getColor(
+            this,
+            R.color.user_background_light,
+        ),
+        onBackground = ContextCompat.getColor(
+            this,
+            R.color.user_on_background_light,
+        ),
+        onSecondaryAction = ContextCompat.getColor(
+            this,
+            R.color.user_on_secondary_action_light,
+        ),
+        error = ContextCompat.getColor(
+            this,
+            R.color.user_error_light,
+        ),
+        notify = ContextCompat.getColor(
+            this,
+            R.color.user_notify_light,
+        ),
+        onError = ContextCompat.getColor(
+            this,
+            R.color.user_on_error_light,
+        ),
+        onNotify = ContextCompat.getColor(
+            this,
+            R.color.user_on_notify_light,
+        ),
+    )
+
+    private fun darkColors(): UserColors = UserColors(
+        primary = ContextCompat.getColor(
+            this, R.color.user_primary_dark,
+        ),
+        onPrimary = ContextCompat.getColor(
+            this,
+            R.color.user_on_primary_dark,
+        ),
+        action = ContextCompat.getColor(
+            this,
+            R.color.user_action_dark,
+        ),
+        onAction = ContextCompat.getColor(
+            this,
+            R.color.user_on_action_dark,
+        ),
+        onMessage = ContextCompat.getColor(
+            this,
+            R.color.user_on_message_dark,
+        ),
+        message = ContextCompat.getColor(
+            this,
+            R.color.user_message_dark,
+        ),
+        onBusinessMessage = ContextCompat.getColor(
+            this,
+            R.color.user_on_business_message_dark,
+        ),
+        businessMessage = ContextCompat.getColor(
+            this,
+            R.color.user_business_message_dark,
+        ),
+        background = ContextCompat.getColor(
+            this,
+            R.color.user_background_dark,
+        ),
+        onBackground = ContextCompat.getColor(
+            this,
+            R.color.user_on_background_dark,
+        ),
+        onSecondaryAction = ContextCompat.getColor(
+            this,
+            R.color.user_on_secondary_action_dark,
+        ),
+        error = ContextCompat.getColor(
+            this,
+            R.color.user_error_dark,
+        ),
+        notify = ContextCompat.getColor(
+            this,
+            R.color.user_notify_dark,
+        ),
+        onError = ContextCompat.getColor(
+            this,
+            R.color.user_on_error_dark,
+        ),
+        onNotify = ContextCompat.getColor(
+            this,
+            R.color.user_on_notify_dark,
+        ),
+    )
 
     override fun onPrepareOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.top_app_bar, menu)
--- zendesk_sdk_demo/app/src/main/java/com/example/zendesk_sdk_demo/MainApplication.kt
+++ zendesk_sdk_demo/app/src/main/java/com/example/zendesk_sdk_demo/MainApplication.kt
@@ -1,8 +1,6 @@
 package com.example.zendesk_sdk_demo
 
 import android.app.Application
-import android.util.Log
-import zendesk.android.Zendesk
 import zendesk.messaging.android.DefaultMessagingFactory
 
 class MainApplication : Application() {
@@ -10,29 +8,10 @@ class MainApplication : Application() {
     override fun onCreate() {
         super.onCreate()
 
-        // https://developer.zendesk.com/documentation/zendesk-web-widget-sdks/sdks/android/getting_started/#3-initialize-the-sdk
-        Zendesk.initialize(this, this.getString(R.string.channel_key), successCallback = { zendesk ->
-            Log.i(LOG_TAG, getString(R.string.msg_init_success))
-            // https://developer.zendesk.com/documentation/zendesk-web-widget-sdks/sdks/android/analytics_tracking
-            // Enable/Disable analytics tracking, enabled by default
-            zendesk.messaging.enableAnalyticsTracking(enabled = true)
-        }, failureCallback = { error ->
-            // Tracking the cause of exceptions in your crash reporting dashboard will help to triage any unexpected failures in production
-            Log.e(LOG_TAG, "${getString(R.string.msg_init_error)}: $error")
-        }, messagingFactory = DefaultMessagingFactory())
-    }
-
-    companion object {
-        private val LOG_TAG = "[${Companion::class.java.simpleName}]"
-
-        /**
-         * Private reference to the instance of [MainApplication], initialized during [onCreate].
-         */
-        private lateinit var instance: MainApplication
-
-        /**
-         * Returns this instance of [MainApplication].
-         */
-        fun get(): MainApplication = instance
+        ZendeskManager.initialize(
+            context = this,
+            channelKey = this.getString(R.string.channel_key),
+            factory = DefaultMessagingFactory(),
+        )
     }
 }
